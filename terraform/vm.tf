7ce664b82e18330ec5a00c081fbd128d8679d611
Lekhana-Gayatri <148931339+Lekhana-Gayatri@users.noreply.github.com>
2024-07-04 22:13:28 +0530

    Update vm.tf

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,31 +1,22 @@
 
 
-resource "aws_instance" "webserver" {
+resource "aws_instance" "slaves" {
   count=2
   ami=var.ami
-  instance_type = "t3.micro"
+  instance_type = "t3.large"
   key_name = var.key
-  security_groups = [aws_security_group.worker_sg.name]
+  security_groups = [aws_security_group.book_store_sg.name]
   tags = {
   name="slaves"
   }
 }
 
-resource "aws_instance" "db" {
-  ami=var.ami
-  instance_type="t3.micro"
-  key_name = var.key
-  security_groups = [aws_security_group.db_sg.name]
-  tags = {
-  name="slave3"
-  }
-}
 
 resource "aws_instance" "master" {
   ami=var.ami
   key_name = var.key
-  security_groups = [aws_security_group.master_sg.name]
-  instance_type= "t3.micro"
+  security_groups = [aws_security_group.book_store_sg.name]
+  instance_type= "t3.large"
   tags = {
   name="master"
   }
@@ -34,8 +25,8 @@ resource "aws_instance" "master" {
 resource "aws_instance" "jenkins-sonar" {
   ami=var.ami
   key_name = var.key
-  security_groups = [aws_security_group.js.name]
-  instance_type= "t3.micro"
+  security_groups = [aws_security_group.book_store_sg.name]
+  instance_type= "t3.large"
   tags = {
   name="jenkins-sonar"
   }
@@ -43,8 +34,8 @@ resource "aws_instance" "jenkins-sonar" {
 resource "aws_instance" "monitor" {
   ami=var.ami
   key_name = var.key
-  security_groups = [aws_security_group.monitor.name]
-  instance_type= "t3.micro"
+  security_groups = [aws_security_group.book_store_sg.name]
+  instance_type= "t3.large"
   tags = {
   name="monitor"
   }
