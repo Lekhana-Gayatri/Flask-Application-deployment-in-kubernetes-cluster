ba130bdb9712e7ca25a0124d27550330e3ad8221
Lekhana-Gayatri <148931339+Lekhana-Gayatri@users.noreply.github.com>
2024-07-04 22:14:50 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,9 @@
 output "webserver_ips" {
-  value =[ aws_instance.webserver[0].public_ip, aws_instance.webserver[1].public_ip]
-}
-output "db_ip" {
-  value = aws_instance.db.public_ip
+  value =[ aws_instance.slaves[0].public_ip, aws_instance.slaves[1].public_ip]
 }
 output "jenkins" {
   value = aws_instance.jenkins-sonar.public_ip
 }
 output "monitor" {
-  value = aws_instance.jenkins-sonar.public_ip
+  value = aws_instance.monitor.public_ip
 }
