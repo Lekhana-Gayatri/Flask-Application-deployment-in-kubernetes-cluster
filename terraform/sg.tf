49a5ef0fbb5222fb46217b3e37277b034f13514b
Lekhana-Gayatri <148931339+Lekhana-Gayatri@users.noreply.github.com>
2024-07-04 22:14:22 +0530

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,236 +1,112 @@
-resource "aws_security_group" "db_sg" {
-  name = "db_Sg"
-}
+resource "aws_security_group" "book_store_sg" {
+  name        = "book-store-sg"
+  description = "Security group for Book Store application"
+  vpc_id      = "vpc-0ffaf563107cbbe79" # replace with your VPC ID
 
-resource "aws_security_group" "master_sg" {
-  name        = "master-sg"
-  description = "Security group for Kubernetes master node"
-}
-resource "aws_security_group" "worker_sg" {
-  name        = "worker-sg"
-  description = "Security group for Kubernetes worker nodes"
-}
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "MYSQL/Aurora"
+  }
 
-resource "aws_security_group_rule" "ssh_w" {
-  security_group_id = aws_security_group.worker_sg.id
-   type = "ingress"
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Restrict to your IP address for SSH access
-}
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH"
+  }
 
-
-  resource "aws_security_group_rule" "http_w" {
-  security_group_id = aws_security_group.worker_sg.id
-  type = "ingress"
-    from_port   = 80
-    to_port     = 80
+  ingress {
+    from_port   = 25
+    to_port     = 25
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "SMTP"
   }
 
-  resource "aws_security_group_rule" "https_w" {
-      security_group_id = aws_security_group.worker_sg.id
-      type = "ingress"
-      from_port   = 443
-    to_port     = 443
+  ingress {
+    from_port   = 3000
+    to_port     = 10000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Custom TCP"
   }
 
-resource "aws_security_group_rule" "kubelet_w" {
-      security_group_id = aws_security_group.worker_sg.id
-      type = "ingress"
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = [ "${aws_instance.master.public_ip}/32" ]
-  }
-resource "aws_security_group_rule" "node_port_w" {
-      security_group_id = aws_security_group.worker_sg.id
-      type = "ingress"
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict to specific IP ranges if needed for NodePort services
-  }
-
-resource "aws_security_group_rule" "runtime_w" {
-      security_group_id = aws_security_group.worker_sg.id
-      type = "ingress"
-    from_port   = 2375
-    to_port     = 2375
-    protocol    = "tcp"
-    cidr_blocks = [ "${aws_instance.master.public_ip}/32" ]
-  }
-
-resource "aws_security_group_rule" "egress_w" {
-      security_group_id = aws_security_group.worker_sg.id
-      type = "egress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-resource "aws_security_group_rule" "ssh_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict to your IP address for SSH access
-  }
-
-resource "aws_security_group_rule" "api_server_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks=["${aws_instance.db.public_ip}/32","${aws_instance.webserver[0].public_ip}/32","${aws_instance.webserver[1].public_ip}/32"]
-  }
-
-resource "aws_security_group_rule" "etcd_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 2379
-    to_port     = 2380
-    protocol    = "tcp"
-    self        = true
-  }
-
-resource "aws_security_group_rule" "kubelet_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
- cidr_blocks = ["${aws_instance.db.public_ip}/32","${aws_instance.webserver[0].public_ip}/32","${aws_instance.webserver[1].public_ip}/32"]
-}
-
-resource "aws_security_group_rule" "scheduler_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 10251
-    to_port     = 10251
-    protocol    = "tcp"
-    self        = true
-  }
-resource "aws_security_group_rule" "controll_manager_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 10252
-    to_port     = 10252
-    protocol    = "tcp"
-    self        = true
-  }
-
-resource "aws_security_group_rule" "egress_m" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "ingress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-resource "aws_security_group_rule" "mysql_d" {
-      security_group_id = aws_security_group.db_sg.id
-      type = "ingress"
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = [ "${aws_instance.master.public_ip}/32" ]
-  }
-
-resource "aws_security_group_rule" "kubelet_d" {
-      security_group_id = aws_security_group.db_sg.id
-      type = "ingress"
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = [ "${aws_instance.master.public_ip}/32" ]
-  }
-
-resource "aws_security_group_rule" "node_port_d" {
-      security_group_id = aws_security_group.db_sg.id
-      type = "ingress"
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Restrict to specific IP ranges if needed for NodePort services
-  }
-
-resource "aws_security_group_rule" "runtime_d" {
-      security_group_id = aws_security_group.db_sg.id
-      type = "ingress"
-    from_port   = 2375
-    to_port     = 2375
-    protocol    = "tcp"
-    cidr_blocks = [ "${aws_instance.master.public_ip}/32" ]
-  }
-
-
-resource "aws_security_group_rule" "egress_d" {
-      security_group_id = aws_security_group.master_sg.id
-      type = "egress"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-resource "aws_security_group" "js" {
-  name = "js-sg"
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Custom TCP"
   }
 
   ingress {
-    from_port   = 9000
-    to_port     = 9000
+    from_port   = 465
+    to_port     = 465
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "SMTPS"
   }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Custom TCP"
+  }
+
+  ingress {
+    from_port   = 30007
+    to_port     = 30007
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Custom TCP"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Custom TCP"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Custom TCP"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_security_group" "monitor" {
-  name = "monitor-sg"
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 9115
-    to_port     = 9115
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "book-store-sg"
   }
 }
